eb2c1c0c171f213a0ec59548ffca471e62d4c36e
RRyannis <137493198+RRyannis@users.noreply.github.com>
2025-12-27 18:32:20 +0200

    leetcode

---
+++ src/evenNumberOfDigits.java
@@ -0,0 +1,20 @@
+public class evenNumberOfDigits {
+    class Solution {
+        public int findNumbers(int[] nums) {
+            int evenDigitCount = 0;
+            int res = 0;
+            for (int i = 0; i < nums.length; i++){
+                while (nums[i ]!= 0){
+                    nums[i] /= 10 ;
+                    res++;
+
+                }
+                if(res%2==0){
+                    evenDigitCount++;
+                }
+                res = 0;
+            }
+            return evenDigitCount;
+        }
+    }
+}
