a2f28b446d9a201cda3d5eedb0d03275ff4ef68b
RRyannis <137493198+RRyannis@users.noreply.github.com>
2026-01-27 11:32:38 +0200

    height checker problem

---
+++ src/HeightChecker.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class HeightChecker {
+    public int heightChecker(int[] heights){
+        int[] expected = heights.clone();
+        Arrays.sort(heights);
+        int count = 0;
+        for (int i = 0; i < heights.length; i++){
+            if (heights[i] != expected[i]) count++;
+        }
+        return count;
+    }
+}
