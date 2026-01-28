b11b34ec608907af1193e13be05055e61cf195cc
RRyannis <137493198+RRyannis@users.noreply.github.com>
2026-01-28 11:44:53 +0200

    find all disappearing numbers

---
+++ src/FindAllDisappearingNumbers.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
+public class FindAllDisappearingNumbers {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        Arrays.sort(nums);
+        int expected = 1;
+
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] == expected){
+                expected++;
+            } else if (nums[i] > expected){
+                result.add(expected);
+                expected++;
+                i--;
+            }
+        }
+        while (expected <= nums.length) {
+            result.add(expected);
+            expected++;
+        }
+        return result;
+    }
+}
