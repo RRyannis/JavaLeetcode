b1b09330cd21a47c35c98a25cdd817fb8b3d6a2b
RRyannis <137493198+RRyannis@users.noreply.github.com>
2026-01-29 11:32:51 +0200

    sortedSquares

--- src/SortedSquares.java
+++ src/SortedSquares.java
@@ -2,7 +2,6 @@ import java.util.Arrays;
 
 public class SortedSquares {
     public int[] sortedSquares(int[] nums) {
-        int temp = 0;
         for (int i = 0; i < nums.length; i++){
             nums[i] = nums[i]*nums[i];
         }
@@ -10,3 +9,5 @@ public class SortedSquares {
         return nums;
     }
 }
+
+
