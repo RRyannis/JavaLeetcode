0650a8292c59d99a91b83be655f6cfe68c30b2c7
RRyannis <137493198+RRyannis@users.noreply.github.com>
2026-01-31 09:47:08 +0200

    sortedSquares better

--- src/SortedSquares.java
+++ src/SortedSquares.java
@@ -1,12 +1,33 @@
 import java.util.Arrays;
-
+//TBA: there's a way to solve this in O(N) with a left, right and current pointer
 public class SortedSquares {
-    public int[] sortedSquares(int[] nums) {
-        for (int i = 0; i < nums.length; i++){
-            nums[i] = nums[i]*nums[i];
+    public static int[] sortedSquares(int[] nums) {
+//        for (int i = 0; i < nums.length; i++){
+//            nums[i] = nums[i]*nums[i];
+//        }
+//        Arrays.sort(nums);
+//        return nums;
+        int[] result = new int[nums.length];
+        int right, left, current;
+        left = 0;
+        right = result.length-1;
+        current = result.length-1;
+        while ( left <= right ){
+            if ( nums[left]*nums[left] > nums[right]*nums[right]){
+                result[current] = nums[left]*nums[left];
+                left++;
+            } else {
+                result[current] =nums[right]*nums[right];
+                right--;
+            }
+            current--;
         }
-        Arrays.sort(nums);
-        return nums;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-5, -3, 0, 1, 10};
+        System.out.println(sortedSquares(nums));
     }
 }
 
